9b8f10e837e8ef1c48d592bbcd9eb1c24872b52a
muhrofi01 <muhrofi01@gmail.com>
2022-11-09 08:42:15 +0700

    membuat function refreshToken, verifyToken, login, logout

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config();
 const app = express();
 
 try {
@@ -10,6 +14,8 @@ try {
     console.log(error);
 }
 
+app.use(cors({credentials:true, origin:'http:localhost:3000'}));
+app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
